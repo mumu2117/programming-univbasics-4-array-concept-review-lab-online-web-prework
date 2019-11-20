7cd316b74e6c7c6165f4c596946a4b0b2c9c924f
algebraic-designer-6218 <jmiller@archersystems.us>
2019-11-20 19:07:16 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,20 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0 
+  while counter < array.length 
+  puts array [counter]
+  counter +=1 
 end
 
 def find_max_value(array)
-  # Add your solution here
+  counter = 0 
+  while counter < array.length 
+  puts array [counter]
+  counter += 1
 end
 
 def find_min_value(array)
-  # Add your solution here
+ counter = 0 
+  while counter < array.length 
+  puts array [counter]
+  counter += 1
 end
