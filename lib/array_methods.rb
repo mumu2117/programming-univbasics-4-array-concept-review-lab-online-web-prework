410e85f32932d292342ca504867c4da7b250c529
algebraic-designer-6218 <jmiller@archersystems.us>
2019-11-20 19:05:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,8 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0 
+  while counter < array.length 
+  puts array [counter]
+  counter +=1 
 end
 
 def find_max_value(array)
